05a604a9b005a5eb204014e9c8f51c399c3eef76
sebas <luminoz0feria@gmail.com>
2026-07-13 19:58:59 -0600

    CORREGIDO Y FINAL

--- Assets/Scrips/FightMananger.cs
+++ Assets/Scrips/FightMananger.cs
@@ -87,8 +87,11 @@ public class FightManager : MonoBehaviour
             {
                 SoundManager.instance.Play(defender.FighterData.deadSoundName);
                 RemoveFighter(defender);
-                DialogSystem.Instance.ShowDialog(attacker.FighterData.fighterName + "wins the fight!");
-                FighterWin(attacker);
+               if (fighters.Count == 1)
+                {
+                    DialogSystem.Instance.ShowDialog(attacker.FighterData.fighterName + " ¡¡wins the fight!! ");
+                    FighterWin(attacker);
+                }
             }
             yield return new WaitForSeconds(1.5f);
         }
